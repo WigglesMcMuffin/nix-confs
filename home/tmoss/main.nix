14a079b52d19cf5b9b2279a45233ad5309ba7c6e
Tipene Moss <teee.moose@gmail.com>
2026-03-10 16:53:59 -0400

    Moved graphical apps out of home/main

--- home/tmoss/main.nix
+++ home/tmoss/main.nix
@@ -1,29 +1,9 @@
 { config, lib, pkgs, pkgs-stable, ... }: let
   username = "tmoss";
 in {
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (pkgs.lib.getName pkg) [
-      "obsidian"
-    ];
 
   nixpkgs.overlays = [
     (final: prev: {
-      clipse = prev.clipse.override rec {
-        buildGoModule = args: pkgs.buildGoModule ( args // {
-          version = "1.2.0";
-          src = final.fetchFromGitHub {
-            owner = "savedra1";
-            repo = "clipse";
-            rev = "v1.2.0";
-            hash = "sha256-17ZzmgXm8aJGG4D48wJv+rBCvJZMH3b2trHwhTqwb8s=";
-          };
-          vendorHash = "sha256-6lOUSRo1y4u8+8G/L6BvhU052oYrsthqxrsgUtbGhYM=";
-          tags = [ "wayland" ];
-          env = {
-            GCO_ENABLED = "0";
-          };
-        });
-      };
       wezterm = prev.wezterm.overrideAttrs (old: rec {
         version = "0-unstable-2026-01-29-overlaypatch";
         src = pkgs.fetchFromGitHub {
@@ -47,12 +27,8 @@ in {
     ];
 
     unstable = with pkgs; [
-      qutebrowser
       fzf
-      rofi
       gawk
-      clipse
-      obsidian
       ripgrep
       go-task
       eza
@@ -74,7 +50,6 @@ in {
 
     sessionVariables = {
       EDITOR = "nvim";
-      BROWSER = "qutebrowser";
       # l10n
       LANG    = "en_US.UTF-8";
       LC_ALL  = "en_US.UTF-8";
@@ -82,8 +57,6 @@ in {
       LESS    = "--tabs=4 --no-init --LONG-PROMPT --ignore-case --quit-if-one-screen --RAW-CONTROL-CHARS";
     };
   };
-
-  services.clipse.enable = true;
 
   programs = {
     home-manager = {
